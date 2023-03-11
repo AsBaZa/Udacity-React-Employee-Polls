20df3dfbae97a27f9e7d1fc2a8c0d971120b1c24
AsBaZa <asier.badiola@outlook.es>
2023-03-11 13:54:13 +0100

    Preparar carga de datos con Redux

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import { useEffect } from "react";
+import { connect } from "react-redux";
+import { handleInitialData } from "../actions/shared";
+
+function App(props) {
+  useEffect(() => {
+    props.dispatch(handleInitialData());
+  }, []);
+
+  return (
+    <div>Starter Code</div>
+  );
+}
+
+export default connect()(App);
